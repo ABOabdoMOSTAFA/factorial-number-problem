2372d04ebde814eb27f95d0a541539eb68c24618
abdomostafa <https://github.com/Abdo-Mostafa2458/flutter_quran_app.git>
2024-09-01 07:14:45 +0300

    +Update function that calculate the factorial number

--- bin/factorial_number.dart
+++ bin/factorial_number.dart
@@ -1,13 +1,17 @@
 import 'dart:io';
+ int calculateFactorial(int number){
+   int result =1;
 
+   for (int i=1; i<=number;i++){//i=1 number=5
+     result *=i;// 1*1=1 , 1*2=2 ,2*3
+   }
+   return result;
+ }
 void main(){
   //5!=1*2*3*4*5
+  print("enter the number");
 int number = int.parse(stdin.readLineSync()); //5
-  int result =1;
-
-  for (int i=1; i<=number;i++){//i=1 number=5
-    result *=i;// 1*1=1 , 1*2=2 ,2*3
-  }
+  int result =calculateFactorial(number);
 
   print(result);
 }
